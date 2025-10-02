4c47b820c93f07152ef6aaadc7876c6eb813ee12
piyushrajyadav <piyushyadavrajyadav@gmail.com>
2025-10-02 13:56:49 +0530

    fix server issue

--- src/components/ui/beams-background.tsx
+++ src/components/ui/beams-background.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useMemo } from "react";
 import { motion } from "motion/react";
 import { cn } from "@/lib/utils";
 
